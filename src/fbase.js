9a06ac2d6bd6b0e4cdcc9dd0d33b0712f133f37b
agnesofdeo <agnesofdeo@gmail.com>
2023-02-20 17:48:23 +0900

    #3.2 Getting the Nweets

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import * as firebase from "firebase/app";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APT_KEY,
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,62 @@
-import React from "react";
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    console.log(dbNweets);
+
+    dbNweets.forEach((document) =>
+      setNweets((prev) => [document.data(), ...prev])
+    );
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID", docRef.id);
+    } catch (error) {
+      console.error("Error adding document:", error);
+    }
+    setNweet();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
